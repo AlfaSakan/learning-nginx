01cf166d8079887551a9a2da246cfa51123f4d8f
alfa sakan <ahmadsakan@ist.id>
2022-08-30 15:34:42 +0700

    build: react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-const App = () => {
+export default function App() {
   return (
-    <div className="App">
-      <h1>Hi Mom</h1>
-    </div>
-  );
-};
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
 
-export default App;
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
